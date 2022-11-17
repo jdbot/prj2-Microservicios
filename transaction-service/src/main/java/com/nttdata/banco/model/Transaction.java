952af519a5b19a91503f3e2a4b5a9775300f84a2
Jorge David Prado Coloma <jpradoco@emeal.nttdata.com>
2022-11-17 18:30:37 -0500

    Agregando checkstyle a service transaction

--- transaction-service/src/main/java/com/nttdata/banco/model/Transaction.java
+++ transaction-service/src/main/java/com/nttdata/banco/model/Transaction.java
@@ -6,10 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.util.Date;
-
+/**
+ * Transaction document.
+ */
 @Getter
 @AllArgsConstructor
 @Setter
